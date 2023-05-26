0c25326f31e5816cd4426681be24a956c74611aa
Sasank.t <sailokeshthatikonda@gmail.com>
2023-05-26 11:41:04 +0530

    react project with two different pages

---
+++ src/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function AboutPage() {
+  return (
+    <div class="sub-heading">
+      <h2>Sasank is good Developer</h2>
+    </div>
+  );
+}
+
+export default AboutPage();
---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Suresh Bala Sir</h1>
+      <p>Kishore is Good Developer</p>
+    </div>
+  );
+}
+
+export default HomePage;
